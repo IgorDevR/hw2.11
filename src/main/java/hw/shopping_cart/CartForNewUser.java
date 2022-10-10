e236d8269bcbdce6ceaefa620e70a730ba7b285d
Igor <47169805+IgorDevR@users.noreply.github.com>
2022-10-11 00:18:59 +0600

    hw

---
+++ src/main/java/hw/shopping_cart/CartForNewUser.java
@@ -0,0 +1,24 @@
+package hw.shopping_cart;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.annotation.SessionScope;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Component
+@SessionScope
+public class CartForNewUser {
+
+  List<CartProduct> cartProducts = new ArrayList<>();
+
+    public List<CartProduct> getCartProducts() {
+        return cartProducts;
+    }
+
+    public void addCartProducts(CartProduct cp) {
+       cartProducts.add(cp);
+    }
+}
